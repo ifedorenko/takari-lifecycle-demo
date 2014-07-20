c7c8d3321c6c91a07c4e203e93c27cba74fa5612
Igor Fedorenko <igor@ifedorenko.com>
2014-07-20 10:26:27 +0400

    use maven-compiler-plugin version 3.1 to skip compilation
    version of maven-compiler-plugin used by maven 3.2.2 apparently does
    not honour skip/skipMain parameters
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- takari-compile-demo/src/main/java/io/takari/demo/compile/AClass.java
+++ takari-compile-demo/src/main/java/io/takari/demo/compile/AClass.java
@@ -1,5 +1,10 @@
 package io.takari.demo.compile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AClass {
 
+  // this proves compiler does indeed use source 1.7 (or better)
+  public List<String> list = new ArrayList<>();
 }
